b5a2fd673343731a0795cdd1c050aab78f061731
Ziao Huang <71763696+amazing-fish@users.noreply.github.com>
2023-03-19 15:37:47 +0800

    Update image_to_puzzle.py

--- image_processing/image_to_puzzle.py
+++ image_processing/image_to_puzzle.py
@@ -1,33 +1,55 @@
 import cv2
 import numpy as np
+import torch
+from torch.autograd import Variable
+from torchvision import transforms
 
-# 神经网络模型导入的示例，这里使用 TensorFlow
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+from model import SudokuCNN  # 假设你已经实现了一个名为SudokuCNN的卷积神经网络模型
 
-# 1. 加载模型
-model = load_model('image_processing/model/sudoku_model.h5')
 
 def preprocess_image(image_path):
-    # 2. 加载图像文件
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    resized_image = cv2.resize(image, (252, 252))  # 调整图像尺寸
+    normalized_image = resized_image / 255.0  # 归一化像素值
+    return normalized_image
 
-    # 3. 预处理图像（例如，转换为灰度图像，调整大小等）
-    # 在这里，你需要根据你的训练数据和神经网络模型进行调整
 
-    # 4. 使用卷积神经网络识别数独问题
-    # 在这里，你需要使用你的神经网络模型进行预测
-    # 例如，使用 TensorFlow 进行预测：
-    prediction = model.predict(img)
+def predict_sudoku(model, image):
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    image_tensor = transform(image)
+    image_tensor = image_tensor.unsqueeze(0)
+    image_variable = Variable(image_tensor)
+    output = model(image_variable)
+    return output
 
-    # 5. 将识别结果转换为符合现有项目的二维数组
-    puzzle = prediction_to_puzzle(prediction)
 
-    return puzzle
-
-def prediction_to_puzzle(prediction):
-    # 在这里，你需要将神经网络模型的输出转换为一个 9x9 的二维数组
-    # 这将取决于你的模型输出格式
+def output_to_puzzle(output):
     puzzle = np.zeros((9, 9), dtype=int)
-    # ...
+    for i in range(9):
+        for j in range(9):
+            cell_probs = output[i * 9 + j].detach().numpy()
+            cell_value = np.argmax(cell_probs)
+            if cell_value != 0:
+                puzzle[i][j] = cell_value
     return puzzle
+
+
+def recognize_sudoku_puzzle(image_path, model_path):
+    model = SudokuCNN()
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    image = preprocess_image(image_path)
+    output = predict_sudoku(model, image)
+    puzzle = output_to_puzzle(output)
+
+    return puzzle
+
+
+if __name__ == "__main__":
+    image_path = "path/to/sudoku_image.jpg"
+    model_path = "model/sudoku_model.pth"
+    result = recognize_sudoku_puzzle(image_path, model_path)
+    print(result)
