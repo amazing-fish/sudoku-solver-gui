3a097c9d44fb408abf6523246477d0227f4f1819
jiao-ling <1097859252@qq.com>
2025-10-04 21:22:08 +0800

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -11,7 +11,7 @@ from torch.utils.data import Dataset
 
 _MEAN = 0.1307
 _STD = 0.3081
-_FONT_PATH = Path("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf")
+_FONT_PATH = Path("C:\ProgramData\kingsoft\office6\omath\DejaVuMathTeXGyre.ttf")
 
 
 @dataclass
